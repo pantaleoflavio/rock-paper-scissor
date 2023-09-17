ad950878083f7fa039f4d3817eeb0a9bd36c1dcc
flavio <flavio.pantaleo@yahoo.com>
2023-09-17 20:35:36 +0200

    first commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+### Rock Paper Scissor (Lizard Spock)
+
+a nice variant of the classic rock paper scissors game, inspired by the TV series Big Bang Theory
+
+HTML/CSS part where the style of the page is built, then Javascript: for each player choice, there is a DOM modification and CPU random choice.
+with a some functions and logic, it is checked the style status of the selected icon, there is a random answer of CPU (with same logic for style change of icons),
+DOM modification for the score update and reset of the game.
---
+++ script.js
@@ -0,0 +1,138 @@
+const playerScoreEl = document.getElementById('player-score');
+const playerChoiceEl = document.getElementById('playerChoice');
+const computerScoreEl = document.getElementById('computer-score');
+const computerChoiceEl = document.getElementById('computerChoice');
+const resultText = document.getElementById('resultText');
+
+const playerRock = document.getElementById('playerRock');
+const playerPaper = document.getElementById('playerPaper');
+const playerScissor = document.getElementById('playerScissor');
+const playerLizard = document.getElementById('playerLizard');
+const playerSpock = document.getElementById('playerSpock');
+
+const computerRock = document.getElementById('computerRock');
+const computerPaper = document.getElementById('computerPaper');
+const computerScissor = document.getElementById('computerScissor');
+const computerLizard = document.getElementById('computerLizard');
+const computerSpock = document.getElementById('computerSpock');
+
+const allGemeIcons = document.querySelectorAll('.far')
+
+const choices = {
+  rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
+  paper: { name: 'Paper', defeats: ['rock', 'spock'] },
+  scissors: { name: 'Scissors', defeats: ['paper', 'lizard'] },
+  lizard: { name: 'Lizard', defeats: ['paper', 'spock'] },
+  spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
+};
+
+let playerScoreNumber = 0;
+let computerScoreNumber = 0;
+let computerChoice = '';
+
+// Reset all selected icons
+function resetSelected() {
+  allGemeIcons.forEach((icon) => {
+    icon.classList.remove('selected');
+  });
+}
+
+// Random CPU choice
+function computerRandomChoice() {
+  const computerChoiceNumber = Math.random();
+  if (computerChoiceNumber < 0.2) {
+    computerChoice = 'rock';
+  } else if (computerChoiceNumber <= 0.4) {
+    computerChoice = 'paper';
+  } else if (computerChoiceNumber <= 0.6) {
+    computerChoice = 'scissor';
+  } else if (computerChoiceNumber <= 0.8) {
+    computerChoice = 'lizard';
+  } else {
+    computerChoice = 'spock';
+  }
+}
+
+// Add selected styling in cpu choice
+function displayComputerChoice() {
+  switch (computerChoice) {
+    case 'rock':
+      computerRock.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Rock '
+      break;
+    case 'paper':
+      computerPaper.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Paper '
+      break;
+    case 'scissors':
+      computerScissor.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Scissor '
+      break;
+    case 'lizard':
+      computerLizard.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Lizard '
+      break;
+    case 'spock':
+      computerSpock.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Spock '
+      break;
+    default:
+      break;
+  }
+}
+
+// check result, increase scores, update result
+function updateScore(playerChoice) {
+  if (playerChoice === computerChoice) {
+    resultText.textContent = "It's a Tie";
+  } else {
+    const choice = choices[playerChoice];
+    if (choice.defeats.indexOf(computerChoice) > -1) {
+      resultText.textContent = "You Won!";
+      playerScoreNumber++;
+      playerScoreEl.textContent = playerScoreNumber;
+    } else {
+      resultText.textContent = "You Loose!";
+      computerScoreNumber++;
+      computerScoreEl.textContent = computerScoreNumber;
+    }
+  }
+}
+
+// Call functions to process turn
+function checkResult(playerChoice) {
+  resetSelected();
+  computerRandomChoice();
+  displayComputerChoice();
+  updateScore(playerChoice);
+}
+
+// passing player selection value and style icon
+function select(playerChoice) {
+  checkResult(playerChoice);
+  // add selected styling and player choice
+  switch (playerChoice) {
+    case 'rock':
+      playerRock.classList.add('selected');
+      playerChoiceEl.textContent = ' --- Rock '
+      break;
+    case 'paper':
+      playerPaper.classList.add('selected');
+      playerChoiceEl.textContent = ' --- Paper '
+      break;
+    case 'scissors':
+      playerScissor.classList.add('selected');
+      playerChoiceEl.textContent = ' --- Scissor '
+      break;
+    case 'lizard':
+      playerLizard.classList.add('selected');
+      playerChoiceEl.textContent = ' --- Lizard '
+      break;
+    case 'spock':
+      playerSpock.classList.add('selected');
+      playerChoiceEl.textContent = ' --- Spock '
+      break;
+    default:
+      break;
+  }
+}
